3905d1c19e618e6cd746d53cef20c0f4ed716e87
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-05-26 19:11:33 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// Function to make navigation bar responsive, change to hamburger when screen size reduces
+function responsiveNav(y) {
+    y.classList.toggle("change");
+    let x = document.getElementById("myNav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  // Lori & Craig Responsive NavBar End 
+  
